765315736f624e57534f7d1c060f934e86d3a338
rachanon khongchai <6210110297@psu.ac.th>
2023-02-10 19:09:35 +0700

    add card color and remember result

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, render_template, request
 from models.message_classifier import MessageClassifier
 
 app = Flask(__name__)
@@ -20,6 +20,14 @@ category_map = {
     'C': 'Common'
 }
 
+card_color_map = {
+    'Q': '#ffff00',
+    'S': '#00bfff',
+    'T': '#0040ff',
+    'A': '#8000ff',
+    'C': '#00ff00'
+}
+
 @app.route('/', methods=['GET'])
 def input_form():
     global text_cache
@@ -36,11 +44,11 @@ def my_form_post():
     text_cache= text
 
     # more readable output
-
     output = classifier.classify(text_input= text)
-    output = [category_map[output[0][-1]], round(output[1], 3)]
+    card_color= card_color_map[output[0][-1]]
+    output = [category_map[output[0][-1]], round(output[1], 3),]
 
-    return render_template('input_form.html', message= text, output= output)
+    return render_template('input_form.html', message= text, output= output, card_color= card_color)
 
 
 if __name__ == "__main__":
